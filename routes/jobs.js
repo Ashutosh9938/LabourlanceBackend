4009e561837e6437b969423d937702363b7cf471
Ashutosh9938 <ashutosh.201752@ncit.edu.np>
2024-06-07 23:42:17 +0545

    image upload update

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
-const {uploadProductMedia, createJob, deleteJob, getAllJobs, updateJob, getJob, showStats, getAllPosts } = require('../controllers/jobs');
+const { createJob, deleteJob, getAllJobs, updateJob, getJob, showStats, getAllPosts } = require('../controllers/jobs');
 const testUser = require('../middleware/testUser');
 
 router.route('/').post(authenticateUser, createJob).get(authenticateUser,getAllPosts)
 router.route('/user').get(authenticateUser,getAllJobs)
-router.route('/uploads').post(authenticateUser,uploadProductMedia);
+// router.route('/uploads').post(authenticateUser,uploadProductMedia);
 router.route('/stats').get(authenticateUser,showStats);
 
 
