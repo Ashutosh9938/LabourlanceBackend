3ecec77bbff70b6d40b5a2b47e8d181cc3ba18bd
Ashutosh9938 <ashutosh.201752@ncit.edu.np>
2024-07-25 00:49:32 +0545

    review add only left work to do

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { auth} = require('../middleware/authentication');
+
+const {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviewController');
+
+router.route('/').post(auth, createReview).get(getAllReviews);
+
+router
+  .route('/:id')
+  .get(getSingleReview)
+  .patch(auth, updateReview)
+  .delete(auth, deleteReview);
+
+module.exports = router;
